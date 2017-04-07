d4efcc92464acc9ef19dc3545d3025125fb17e62
Shelomentsev Dmitriy <shelomentsev@protonmail.ch>
2017-04-07 23:30:52 +0300

    TL_config decoding added

--- core/api.go
+++ core/api.go
@@ -51,7 +51,76 @@ func (m *DecodeBuf) ObjectGenerated(constructor uint32) (r TL) {
 	case crc_null:
 		r = TL_null{}
 	case crc_config:
-		// TODO: TL_config
+		flags := m.Int()
+		phonecalls_enabled := false
+		if flags & ( 1 << 1 ) {
+			phonecalls_enabled = true
+		}
+		date := m.Int()
+		expires := m.Int()
+		test_mode := m.ObjectGenerated(m.UInt())
+		this_dc := m.Int()
+		dc_options := m.Vector()
+		chat_size_max := m.Int()
+		megagroup_size_max := m.Int()
+		forwarded_count_max := m.Int()
+		online_update_period_ms := m.Int()
+		offline_blur_timeout_ms := m.Int()
+		offline_idle_timeout_ms := m.Int()
+		online_cloud_timeout_ms := m.Int()
+		notify_cloud_delay_ms := m.Int()
+		notify_default_delay_ms := m.Int()
+		chat_big_size := m.Int()
+		push_chat_period_ms := m.Int()
+		push_chat_limit := m.Int()
+		saved_gifs_limit := m.Int()
+		edit_time_limit := m.Int()
+		rating_e_decay := m.Int()
+		stickers_recent_limit := m.Int()
+		var tmp_sessions int32
+		if flags & (1 << 0) {
+			tmp_sessions = m.Int()
+		}
+		pinned_dialogs_count_max := m.Int()
+		call_receive_timeout_ms := m.Int()
+		call_ring_timeout_ms := m.Int()
+		call_connect_timeout_ms := m.Int()
+		call_packet_timeout_ms := m.Int()
+		me_url_prefix := m.String()
+		disabled_features := m.Vector()
+		r = TL_config{
+			flags: flags,
+			phonecalls_enabled: phonecalls_enabled,
+			date: date,
+			expires: expires,
+			test_mode: test_mode,
+			this_dc: this_dc,
+			dc_options: dc_options,
+			chat_size_max: chat_size_max,
+			megagroup_size_max: megagroup_size_max,
+			forwarded_count_max: forwarded_count_max,
+			online_update_period_ms: online_update_period_ms,
+			offline_blur_timeout_ms: offline_blur_timeout_ms,
+			offline_idle_timeout_ms: offline_idle_timeout_ms,
+			online_cloud_timeout_ms: online_cloud_timeout_ms,
+			notify_cloud_delay_ms: notify_cloud_delay_ms,
+			notify_default_delay_ms: notify_default_delay_ms,
+			chat_big_size: chat_big_size,
+			push_chat_period_ms: push_chat_period_ms,
+			push_chat_limit: push_chat_limit,
+			saved_gifs_limit: saved_gifs_limit,
+			edit_time_limit: edit_time_limit,
+			rating_e_decay: rating_e_decay,
+			stickers_recent_limit: stickers_recent_limit,
+			tmp_sessions: tmp_sessions,
+			pinned_dialogs_count_max: pinned_dialogs_count_max,
+			call_receive_timeout_ms: call_receive_timeout_ms,
+			call_ring_timeout_ms: call_ring_timeout_ms,
+			call_connect_timeout_ms: call_connect_timeout_ms,
+			call_packet_timeout_ms: call_packet_timeout_ms,
+			me_url_prefix: me_url_prefix,
+			disabled_features: disabled_features,
+		}
 	case crc_dcOption:
 		// TODO: TL_dcOption
 	default:
@@ -120,7 +189,7 @@ const crc_config = 0xcb601684
 
 type TL_config struct {
 	flags                    int32
-	phonecalls_enabled       TL // flags.1?true TODO: TL_true
+	phonecalls_enabled       bool // flags.1?true TODO: TL_true
 	date                     int32
 	expires                  int32
 	test_mode                TL // TL_boolFalse or TL_boolTrue
@@ -148,7 +217,7 @@ type TL_config struct {
 	call_ring_timeout_ms     int32
 	call_connect_timeout_ms  int32
 	call_packet_timeout_ms   int32
-	me_url_prefix            int32
+	me_url_prefix            string
 	disabled_features        []TL // DisabledFeature
 }
 
